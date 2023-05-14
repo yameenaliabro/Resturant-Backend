6d1a748121119dd346c5a8b82d153de648000e75
yameenaliabro <muhammadyameen5432@gmail.com>
2023-05-14 12:38:43 +0500

    fixed cors issue

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 let express = require("express")
 let app = express()
 let path = require("path")
-let corse = require("cors")
 let bodyparser  = require("body-parser");
 const mongoose = require("./db/models/connection");
 let cors = require("cors");
@@ -9,14 +8,13 @@ const authrouter = require("./router/auth");
 const product = require("./router/Product");
 const order = require("./router/Order");
 const user = require("./router/User");
+app.use(cors())
 app.use(bodyparser.urlencoded({extended: false}))
 app.use(bodyparser.json());
 app.use(express.static(path.join(process.cwd(),"public")))
 app.use('/thumbnails', express.static(path.join(__dirname, 'public', 'thumbnails')));
-app.use(corse())
 app.set("view engine", "ejs")
 app.set("views","views")
-app.use(corse())
 app.use("/auth",authrouter)
 app.use("/food",product);
 app.use("/order",order)
